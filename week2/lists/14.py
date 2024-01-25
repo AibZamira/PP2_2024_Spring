ddf8f29515059724157e7ac05011e09adedbffdc
AibZamira <a_zamira@kbtu.kz>
2024-01-25 22:34:51 +0600

    'upd'

---
+++ week2/lists/14.py
@@ -0,0 +1,12 @@
+mylist = ["apple", "banana", "cherry"]
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+
+print(thislist)
+print(thislist == mylist)
+print(len(thislist))
+
+list1 = ["abc", 34, True, 40, "male"]
+print(type(mylist))
+
+list1 = list(("apple", "banana", "cherry"))
