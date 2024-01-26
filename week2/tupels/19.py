d4014c8b060e316ee3f2b495cdc9606933869730
AibZamira <a_zamira@kbtu.kz>
2024-01-26 15:37:18 +0600

    'upd'

---
+++ week2/tupels/19.py
@@ -0,0 +1,16 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+print(len(thistuple))
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+thistuple = ("apple")
+print(type(thistuple))
+
+thistuple = tuple(("apple", "banana", "cherry"))
+print(thistuple)
