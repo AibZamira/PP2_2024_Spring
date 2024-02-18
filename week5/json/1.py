e8e7ce25fd53186a56739b942680daabb797322c
AibZamira <a_zamira@kbtu.kz>
2024-02-18 18:53:29 +0600

    'upd'

---
+++ week5/json/1.py
@@ -0,0 +1,10 @@
+import json
+print("Interface Status")
+print("================================================================================")
+print("DN                                                 Description           Speed    MTU  ")
+print("-------------------------------------------------- --------------------  ------  ------")
+
+with open("sample-data.json", "r") as js_file:
+    j = json.load(js_file)
+for it in j["imdata"]:
+    print(f"{it["l1PhysIf"]["attributes"]["dn"]}                              {it["l1PhysIf"]["attributes"]["speed"]}   {it["l1PhysIf"]["attributes"]["mtu"]}")
