229dc593a8c446d9fdf5609bc766079906472b56
AibZamira <a_zamira@kbtu.kz>
2024-02-25 13:28:45 +0600

    'upd'

---
+++ week6/RegEx/10.py
@@ -0,0 +1,17 @@
+import re
+def S(match_obj):
+    return  "_" + match_obj.group('Z').casefold()
+
+def test(pattern, testData, testNumber, expectedResult):
+    result = re.sub(pattern, S, testData)
+    pattern1 = r"\b^_"
+    result = re.sub(pattern1, "", result)
+    print(result)
+    if result == expectedResult:
+        print(testNumber + " is passed!")
+    else: 
+        print(testNumber + " is not passed!")
+        
+pattern = r'(?P<Z>[A-Z][a-z]*)'
+test(pattern, "HelloWorld", "test1", "hello_world")
+test(pattern, "MySuperTestForYou", "test1", "my_super_test_for_you")
