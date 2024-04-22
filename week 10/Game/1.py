df9487434db740d8f8c840bd8667a4973540da09
AibZamira <a_zamira@kbtu.kz>
2024-04-23 00:52:00 +0500

    'upd'

--- week 10/Game/1.py
+++ week 10/Game/1.py
@@ -9,102 +9,92 @@ import random
 import time
 
 conn = psycopg2.connect(
-  host = 'localhost',
-  database = 'postgres',
-  user = 'postgres',
-  password = '76141Zam'
+    host='localhost',
+    database='postgres',
+    user='postgres',
+    password='76141Zam'
 )
 
-cur = conn.cursor()
+
+def initialize_game():
+    pygame.init()
+    screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
+    pygame.display.set_caption('Welcome to the Game')
+    return screen
 
 
-pygame.init()
-screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
-pygame.display.set_caption('Welcome to the Game')
-
-
-def login_menu():
+def login_menu(screen):
     main_theme = pygame_menu.themes.THEME_BLUE
     menu = pygame_menu.Menu('Welcome', 600, 700, theme=main_theme)
-
-    def sign_in():
-        checking()
-    text = "Please, Sign in"
-    menu.add.label(text) 
-    menu.add.button('Start', sign_in)
-    
+    menu.add.label("Welcome to the game!")
+    menu.add.button('Start', checking)
     menu.mainloop(screen)
 
 
 def checking():
     main_theme = pygame_menu.themes.THEME_BLUE
     menu = pygame_menu.Menu('Menu', 600, 700, theme=main_theme)
-    
+    menu.add.label("Please, sign in")
+    login = menu.add.text_input('Login: ', default='')
+    password = menu.add.text_input('Password: ', default='', password=True)
 
-    def checking_usInfo(login, password):
-        user_data = get_user_by_login_and_password(login, password)
-        if(user_data is None):
+    def checking_usInfo():
+        user_data = get_user_by_login_and_password(login.get_value(), password.get_value())
+        if user_data is None:
             checking()
         else:
-            game_menu(login)
+            game_menu(login.get_value())
 
     def get_user_by_login_and_password(login, password):
+        cur = conn.cursor()
         select_query = f"select * from test_users where login = '{login}' and password = '{password}';"
         cur.execute(select_query)
         return cur.fetchone()
-    def p():
-        sign_up()
 
-    login = menu.add.text_input('Login: ', default='')
-    password = menu.add.text_input('Password: ', default='', password=True)
-    menu.add.button('Sign in', lambda: checking_usInfo(login.get_value(), password.get_value()))
-    menu.add.button('Sign up', p)
+    menu.add.button('Sign in', checking_usInfo)
+    menu.add.button('Sign up', sign_up)
     menu.mainloop(screen)
-
 
 
 def sign_up():
     main_theme = pygame_menu.themes.THEME_BLUE
     menu = pygame_menu.Menu('Menu', 600, 700, theme=main_theme)
-    
-    
-    def checking_usInfo(login, password):
+
+    def checking_usInfo():
         insert_query = f"insert into snake_users (login, password) values ('{login}', '{password}');"
+        print("User added sucssesefully!")
         cur.execute(insert_query)
         conn.commit()
         checking()
 
     login = menu.add.text_input('New login: ', default='')
     password = menu.add.text_input('New password: ', default='', password=True)
-    menu.add.button('Sign up', lambda: checking_usInfo(login.get_value(), password.get_value()))
+    menu.add.button('Sign up', checking_usInfo)
     menu.mainloop(screen)
-
 
 
 def start_the_game(login):
     font = pygame.font.SysFont("comicsansms", 30)
-
     clock = pygame.time.Clock()
     speed = settings.SPEED
     running = True
-
-    # Initialization
     snake_obj = Snake()
     fruit_obj = Fruit()
     blocks_group = pygame.sprite.Group()
-
-    step = 4  # the number of fruits after which start new level
+    step = 4
     counter = 1
     level_counter = 0
     cnt = 0
-
-    # create background
     field = pygame.image.load('images/field.png')
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_SPACE]:
+            pause(screen, font)
 
         screen.blit(field, (0, 0))
         score = font.render("Score: " + str(cnt) + "   Level: " + str(level_counter), True, (255, 255, 255))
@@ -116,29 +106,24 @@ def start_the_game(login):
         snake_obj.changeSpeed(speed)
         running = snake_obj.move(counter)
 
-        # Adding new block
         if (counter - 1) > 0 and (counter - 1) % step == 0:
             for _ in range(1):
                 new_block = Block()
                 blocks_group.add(new_block)
-
-            # condition of adding new block
             step += step
             level_counter += 1
             speed += 0.005
 
-        if snake_obj.time > 180:  # timer for food
+        if snake_obj.time > 180:
             fruit_obj.respawn()
             snake_obj.time = 0
 
-        # Eat food
         if (snake_obj.head_rect.colliderect(fruit_obj.rect)):
             fruit_obj.__init__()
             cnt += random.randint(1, 3)
             counter += 1
             snake_obj.time = 0
 
-        # Check collisions
         for block in blocks_group:
             if fruit_obj.rect.colliderect(block.rect):
                 fruit_obj.respawn()
@@ -154,40 +139,66 @@ def start_the_game(login):
                 time.sleep(0.5)
                 running = False
 
-
         pygame.display.flip()
         clock.tick(settings.FPS)
 
     game_over(login, cnt, level_counter)
-    return
+
+
+def pause(screen, font):
+    paused = True
+    clock = pygame.time.Clock()
+    while paused:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        print_text(screen, font, 'Paused. Press enter to continue', 150, 350)
+
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_RETURN]:
+            paused = False
+        
+        pygame.display.update()
+        clock.tick(15)
+
+def print_text(screen, font, message, x, y, font_color=(0, 0, 0)):
+    text = font.render(message, True, font_color)
+    screen.blit(text, (x, y))
+
 
 
 def game_menu(login):
+    cur = conn.cursor()
     image = pygame.image.load("images/field.png")
     main_theme = pygame_menu.themes.THEME_BLUE
-
     menu = pygame_menu.Menu('Menu', 600, 700, theme=main_theme)
-    
-    
+
     def quit_the_game():
-        print('Quitting the game!')
         pygame.quit()
         quit()
-
 
     select_query = f"select score, level from snake_users where login = '{login}';"
     cur.execute(select_query)
     p = cur.fetchall()
-    print(p[0][0])
 
-    menu.add.label(f"Hello {login}")
-    menu.add.label(f"Score: {p[0][0]}   level: {p[0][1]}")
+    if p:
+        score_label = f"Score: {p[0][0]}" 
+        level_label = f"Level: {p[0][1]}"
+    else:
+        score_label = "Score: 0"
+        level_label = "Level: 0"
+
+    menu.add.label(f"Hello {login}!")
+    menu.add.label(score_label + "   " + level_label)
     menu.add.button('Play', lambda: start_the_game(login))
     menu.add.button('Exit', quit_the_game)
     menu.mainloop(screen)
-    
+
 
 def game_over(login, score, level):
+    cur = conn.cursor()
     main_theme = pygame_menu.themes.THEME_BLUE
     menu = pygame_menu.Menu('Game Over', 600, 700, theme=main_theme)
     update_query = f"update snake_users set score = {score}, level = {level} where login = '{login}';"
@@ -205,8 +216,10 @@ def game_over(login, score, level):
     menu.add.label(f'Score: {score}   Level: {level}')
     menu.add.button('Start Again', start_game_again)
     menu.add.button('Quit', quit_game)
-
     menu.mainloop(screen)
 
-login_menu()
-conn.close()
+
+if __name__ == "__main__":
+    screen = initialize_game()
+    login_menu(screen)
+    conn.close()
