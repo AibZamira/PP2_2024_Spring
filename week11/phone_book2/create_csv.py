c11339f4791dbefe0674901d2f154a7316f10ac5
AibZamira <a_zamira@kbtu.kz>
2024-04-27 02:12:32 +0500

    'upd'

---
+++ week11/phone_book2/create_csv.py
@@ -0,0 +1,12 @@
+import csv
+
+filename = 'phone_book.csv'
+
+
+with open(filename, 'w', newline='',encoding="utf8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Name', ' Phone'])
+    writer.writerow(['Anna',870712345])
+    writer.writerow(['Ivan',874701010])
+    writer.writerow(['Tomirsis',870100035])
+    writer.writerow(['Ayan',877755555])
